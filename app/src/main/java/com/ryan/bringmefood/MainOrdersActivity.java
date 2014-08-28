8a395dc434e7ee9e3d9537a5e9d181c2595a4978
Ryan D'souza <dsouzarc@gmail.com>
2014-08-28 17:57:59 -0400

    Working Version

--- app/src/main/java/com/ryan/bringmefood/MainOrdersActivity.java
+++ app/src/main/java/com/ryan/bringmefood/MainOrdersActivity.java
@@ -5,13 +5,39 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.io.InputStream;
 
-public class MainOrdersActivity extends Activity {
+public class MainOrdersActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_orders);
+
+        
     }
 
 
