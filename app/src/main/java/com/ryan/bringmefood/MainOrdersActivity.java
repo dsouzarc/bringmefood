28fe001c9e620983a6c7c4df0688b951d97e8dec
Ryan D'souza <dsouzarc@gmail.com>
2014-09-13 22:43:01 -0400

    Fixed null pointer exception

--- app/src/main/java/com/ryan/bringmefood/MainOrdersActivity.java
+++ app/src/main/java/com/ryan/bringmefood/MainOrdersActivity.java
@@ -34,6 +34,10 @@ public class MainOrdersActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_orders);
 
+        theC = getApplicationContext();
+        thePrefs = this.getSharedPreferences("com.ryan.bringmefood", Context.MODE_PRIVATE);
+        theEd = thePrefs.edit();
+
         Parse.initialize(this, "AdzOc2Rwa3OHorbxpc8mv698qtl8e7dg2XSjscqO",
                 "JY1hNJ9EgxcZxFUp1VlLhqV4ZYd7Azf1H4tQTBQX");
         PushService.setDefaultPushCallback(this, MainOrdersActivity.class);
@@ -42,10 +46,6 @@ public class MainOrdersActivity extends FragmentActivity {
             PushService.subscribe(getApplicationContext(), "Drivers", MainOrdersActivity.class);
             ParseInstallation.getCurrentInstallation().saveInBackground();
         }
-
-        theC = getApplicationContext();
-        thePrefs = this.getSharedPreferences("com.ryan.bringmefood", Context.MODE_PRIVATE);
-        theEd = thePrefs.edit();
 
         theActionBar = getActionBar();
         getActionBar().setIcon(
