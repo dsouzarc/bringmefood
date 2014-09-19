d243a9b8d1e66ee6cc3c93c9b600665e89071bd1
Ryan D'souza <dsouzarc@gmail.com>
2014-09-18 20:56:02 -0400

    Parse crashing fixes

--- app/src/main/java/com/ryan/bringmefood/MainOrdersActivity.java
+++ app/src/main/java/com/ryan/bringmefood/MainOrdersActivity.java
@@ -34,12 +34,18 @@ public class MainOrdersActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_orders);
 
+        try {
+            Parse.initialize(getApplicationContext(), "H7vwuy3u4duhsYm9MyVMi0f1riIs6aixBLVD551V",
+                    "P16oPFyMpAaAsWBUC41XkUCmSkVIS8TA0fUIavkM");
+            PushService.setDefaultPushCallback(getApplicationContext(), MainOrdersActivity.class);
+            ParseInstallation.getCurrentInstallation().saveInBackground();
+        }
+        catch (Exception e) {
+        }
+
         theC = getApplicationContext();
         thePrefs = this.getSharedPreferences("com.ryan.bringmefood", Context.MODE_PRIVATE);
         theEd = thePrefs.edit();
-
-        Parse.initialize(this, "AdzOc2Rwa3OHorbxpc8mv698qtl8e7dg2XSjscqO", "JY1hNJ9EgxcZxFUp1VlLhqV4ZYd7Azf1H4tQTBQX");
-        PushService.setDefaultPushCallback(this, MainOrdersActivity.class);
 
         theActionBar = getActionBar();
         getActionBar().setIcon(
