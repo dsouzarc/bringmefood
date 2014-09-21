7d2e12a1cd7ca8d88e342c83501edf4ad7417479
Ryan D'souza <dsouzarc@gmail.com>
2014-09-21 12:22:07 -0400

    Layout for each menu item

--- app/src/main/java/com/ryan/bringmefood/MenuListViewAdapter.java
+++ app/src/main/java/com/ryan/bringmefood/MenuListViewAdapter.java
@@ -1,7 +1,40 @@
 package com.ryan.bringmefood;
 
-/**
- * Created by Ryan on 9/21/14.
- */
-public class MenuListViewAdapter {
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.List;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+public class MenuListViewAdapter extends ArrayAdapter<MenuItem> {
+    private final Context theC;
+    private final LayoutInflater inflater;
+    private final MenuItem[] theMenu;
+    private final SparseBooleanArray mSelectedItemsIds;
+
+    public MenuListViewAdapter(Context context, int resource, MenuItem[] objects) {
+        super(context, resource, objects);
+        this.theC = context;
+        this.inflater = LayoutInflater.from(context);
+        this.theMenu = objects;
+        this.mSelectedItemsIds = new SparseBooleanArray();
+    }
+
+    private class ViewHolder {
+        TextView itemName;
+        TextView itemDescription;
+        TextView itemCost;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        final ViewHolder holder;
+        if(view == null) {
+            holder = new ViewHolder();
+            view = inflater.inflate(R.layout.lis)
+    }
 }
