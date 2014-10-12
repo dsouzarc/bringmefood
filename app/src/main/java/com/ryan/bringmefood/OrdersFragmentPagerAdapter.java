0a3d5f2393e378a92afd8a24be02f1e89cc3ab6c
Ryan D'souza <dsouzarc@gmail.com>
2014-10-11 20:40:52 -0400

    Spinner

--- app/src/main/java/com/ryan/bringmefood/OrdersFragmentPagerAdapter.java
+++ app/src/main/java/com/ryan/bringmefood/OrdersFragmentPagerAdapter.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.widget.Spinner;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
